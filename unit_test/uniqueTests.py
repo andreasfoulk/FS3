09610e71d4fdd70a113d78d0b8ae63dd85dfdf81
mduzac <mduzac@mymail.mines.edu>
2018-05-31 10:06:21 -0600

    Unique Class now handling empty cells properly

--- unit_test/uniqueTests.py
+++ unit_test/uniqueTests.py
@@ -24,6 +24,10 @@ class UniqueValueTests(unittest.TestCase):
         self.goodValues = FS3Uniqueness()
         self.goodValues.initialize([1,2,1,2,1,2,3])
 
+        #A set of data with empty values
+        self.emptyValues = FS3Uniqueness()
+        self.emptyValues.initialize([1,2,1,None,2,None,1,2])
+
     def testUniqueGoodValues(self):
         expected = [1,2,3]
         actual = self.goodValues.uniqueValues
@@ -37,6 +41,21 @@ class UniqueValueTests(unittest.TestCase):
     def testUniqueGoodValuesPercent(self):
         expected = [42.857142857142854, 42.857142857142854, 14.285714285714285]
         actual = self.goodValues.uniquePercent
+        self.assertEqual(actual, expected)
+
+    def testUniqueEmptyValues(self):
+        expected = [1,2,'[Empty]']
+        actual = self.emptyValues.uniqueValues
+        self.assertEqual(actual, expected)
+
+    def testUniqueEmptyValuesCounts(self):
+        expected = [3,3,2]
+        actual = self.emptyValues.uniqueNumOccur
+        self.assertEqual(actual, expected)
+
+    def testUniqueEmptyValuesPercent(self):
+        expected = [37.5, 37.5, 25.0]
+        actual = self.emptyValues.uniquePercent
         self.assertEqual(actual, expected)
 
 
