a9910f7cd9be7aa7daa3f4a058de86b34dc30c49
mduzac <mduzac@mymail.mines.edu>
2018-05-31 09:54:12 -0600

    Unique Functions pass with simple data input

--- unit_test/uniqueTests.py
+++ unit_test/uniqueTests.py
@@ -10,7 +10,7 @@ TODO:
 
 #These imports are required for unit tests
 import unittest
-from roundFunc import decimalRound
+from fs3Unique import FS3Uniqueness
 
 class UniqueValueTests(unittest.TestCase):
     """
@@ -18,14 +18,25 @@ class UniqueValueTests(unittest.TestCase):
     Tests for our unique tab
     """
 
-    def testUnique_0(self):
-        """
-        Perfect Data
-        """
-        inputNum = 1
-        numDecimals = 2
-        expected = 1.00
-        actual = decimalRound(inputNum, numDecimals)
+    @classmethod
+    def setUpClass(self):
+        #A simple set of data with no empties
+        self.goodValues = FS3Uniqueness()
+        self.goodValues.initialize([1,2,1,2,1,2,3])
+
+    def testUniqueGoodValues(self):
+        expected = [1,2,3]
+        actual = self.goodValues.uniqueValues
+        self.assertEqual(actual, expected)
+
+    def testUniqueGoodValuesCounts(self):
+        expected = [3,3,1]
+        actual = self.goodValues.uniqueNumOccur
+        self.assertEqual(actual, expected)
+
+    def testUniqueGoodValuesPercent(self):
+        expected = [42.857142857142854, 42.857142857142854, 14.285714285714285]
+        actual = self.goodValues.uniquePercent
         self.assertEqual(actual, expected)
 
 
