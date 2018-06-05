fd5dc72aef291ae1c609eaf2f554d3e34b4cbc62
Orden Aitchedji <oaitchedji@mines.edu>
2018-06-05 16:15:36 -0600

    French Language setup

---
+++ i18n/fs3.pro
@@ -0,0 +1,3 @@
+FORMS = ../fs3.ui
+SOURCES = ../fs3Run.py
+TRANSLATIONS = fs3_fr.ts
---
+++ i18n/fs3_fr.ts
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="en_US">
+<context>
+    <name>mainWindow</name>
+    <message>
+        <location filename="../fs3.ui" line="20"/>
+        <source>MainWindow</source>
+        <translatorcomment>Fenêtre principale</translatorcomment>
+        <translation>FenêtrePrincipale</translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="62"/>
+        <source>Select Layer</source>
+        <translatorcomment>Choisir/Sélectionner une couche</translatorcomment>
+        <translation>Sélectionner une couche</translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="76"/>
+        <source>Select Fields</source>
+        <translatorcomment>Sélectionnez le champ</translatorcomment>
+        <translation>Sélectionnez le champ</translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="90"/>
+        <source>Limit to Selected</source>
+        <translatorcomment>Limiter le champ de sélection</translatorcomment>
+        <translation>Limite sélection</translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="102"/>
+        <source>Number of Decimals</source>
+        <translatorcomment>Nombre de décimales à afficher</translatorcomment>
+        <translation>Nombre de décimales</translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="128"/>
+        <source>Percentiles</source>
+        <translation>Centile</translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="146"/>
+        <source>25%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="153"/>
+        <source>10%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="164"/>
+        <source>5%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="171"/>
+        <source>High End</source>
+        <translatorcomment>Extrémité supérieure</translatorcomment>
+        <translation>Haut Extrémité</translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="197"/>
+        <source>Previous</source>
+        <translation>Précédent</translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="204"/>
+        <source>Next</source>
+        <translation>Suivant</translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="222"/>
+        <source>Data Table</source>
+        <translation>Tableau de Données</translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="227"/>
+        <source>Statistics</source>
+        <translation>Statistique</translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="232"/>
+        <source>Unique</source>
+        <translation>Unique</translation>
+    </message>
+    <message>
+        <location filename="../fs3.ui" line="237"/>
+        <source>Graph</source>
+        <translation>Graphique</translation>
+    </message>
+</context>
+</TS>
