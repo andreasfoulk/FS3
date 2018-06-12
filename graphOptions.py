759ba3b6b69aab904ac2b9b4c7f5fed94d5c4acf
Tanner Lee <tannerlee@mymail.mines.edu>
2018-06-12 14:24:25 -0600

    Added the FS3 icon to the graph options window.

--- graphOptions.py
+++ graphOptions.py
@@ -3,8 +3,11 @@ generic header
 """
 import os
 
+from .resources import *
+
 from PyQt5 import uic
 from PyQt5.QtWidgets import QDialog
+from PyQt5.QtGui import QIcon
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'graphOptions.ui'))
@@ -17,6 +20,8 @@ class GraphOptionsWindow(QDialog, FORM_CLASS):
         super(GraphOptionsWindow, self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle("Graph Options")
+        self.iconPath = ":/plugins/FS3/FS3Icon.png"
+        self.setWindowIcon(QIcon(self.iconPath))
 
         transforms = ['None', 'Log']
         self.dataTransformBox.insertItems(0, transforms)
