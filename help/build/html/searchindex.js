be2d22e4da3abca9d5ddfe598d9342fef241fd89
mduzac <mduzac@mymail.mines.edu>
2018-06-12 14:35:27 -0600

    removed help folder without error

--- help/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to FieldStats\u2019s documentation!"],titleterms:{document:0,fieldstat:0,indic:0,tabl:0,welcom:0}})
